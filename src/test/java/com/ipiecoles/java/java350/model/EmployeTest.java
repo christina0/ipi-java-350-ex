2817d3b5815d1c200b48f98d04ea0f90ad5c2af3
christina0 <53077918+christina0@users.noreply.github.com>
2021-11-22 15:29:25 +0100

    employee date paste

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,41 @@
+package com.ipiecoles.java.java350.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeTest {
+
+    @Test
+    void getNombreAnneeAncienneteWithDataEmbaucheNow() {
+        //Given
+        Employe employe = new Employe("Doe", "John", "T12345", LocalDate.now().plusyears(6), 2500.0, 1, 1.0);
+
+        //When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        //Then
+        Assertions.assertThat(nbAnnees).isEqualTo(0);
+    }
+
+    @Test
+    void getNombreAnneeAncienneteWithPastDateEmbauche(){
+        //Given
+        Employe employe = new Employe("Doe", "John", "T12345", LocalDate.now().minusYears(6), 2500.0, 1, 1.0);
+        //When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        //Then
+        Assertions.assertThat(nbAnnees).isEqualTo(6);
+    }
+
+    @Test
+    void getNombreAncienneteWithDateEmbaucheNull() {
+        //Given
+        Employe employe = new Employe("Doe", "John", "T12345", null, 2500.0, 1, 1.0);
+        //When
+        Integer nbAnnees = employe.getNombreAnneeAnciennete();
+        //Then
+        Assertions.assertThat(nbAnnees).isNull();
+    }
+
+}
